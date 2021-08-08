77eb738b28037870a25a2ec8c8ff0428684ea179
Gabriel Arcanjo dos Santos Ferreira <gabriel.ferreira@bancointer.com.br>
2021-08-07 23:45:23 -0300

    Add exe02

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main()
+{
+   int R, V, T, D, L;
+
+   // Leitura das entradas
+   scanf("%i %i %i", &T, &V, &R);
+   D = V * T;
+   L = D / R;
+
+   // Print do resultado
+   printf("R = %i\nV = %i\nT = %i\nD = %i\nL = %i\n", R, V, T, D, L);
 }
