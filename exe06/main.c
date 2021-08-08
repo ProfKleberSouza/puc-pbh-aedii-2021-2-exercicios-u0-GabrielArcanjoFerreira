d33307c8e5292e4dc3eb8d2310835c2a6202685a
Gabriel Arcanjo dos Santos Ferreira <gabriel.ferreira@bancointer.com.br>
2021-08-08 20:30:25 -0300

    Add exe06

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+int main()
+{
+   int l1, l2, l3;
+
+   // Ler entradas
+   scanf("%i %i %i", &l1, &l2, &l3);
+
+   // Verificar se triângulo
+   if (l1 < (l2 + l3) && l2 < (l1 + l3) && l3 < (l2 + l1))
+   {
+      // Se triângulo
+      if (l1 == l2 && l2 == l3)
+      {
+         // Equilátero
+         printf("TRIANGULO EQUILATERO");
+      }
+      else if (l1 == l2 || l1 == l3 || l2 == l3)
+      {
+         // Isósceles
+         printf("TRIANGULO ISOSCELES");
+      }
+      else
+      {
+         printf("TRIANGULO ESCALENO");
+      }
+   }
+   else
+   {
+      printf("OS LADOS NAO FORMAM UM TRIANGULO");
+   }
+
    return 0;
 }
