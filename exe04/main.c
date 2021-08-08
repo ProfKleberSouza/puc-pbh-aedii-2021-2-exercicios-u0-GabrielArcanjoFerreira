46de90666ad5e66becd4eabb2fc6ceab36da48dd
Gabriel Arcanjo dos Santos Ferreira <gabriel.ferreira@bancointer.com.br>
2021-08-08 00:32:50 -0300

    Add exe04

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+int main()
+{
+   int n1, n2, n3, maior, menor;
+
+   // Ler entradas
+   scanf("%i %i %i", &n1, &n2, &n3);
+
+   // Verificar maior e menor
+   if (n1 >= n2 && n1 >= n3)
+   {
+      maior = n1;
+   }
+   else if (n2 >= n1 && n2 >= n3)
+   {
+      maior = n2;
+   }
+   else if (n3 >= n1 && n3 >= n2)
+   {
+      maior = n3;
+   }
+
+   if (n1 <= n2 && n1 <= n3)
+   {
+      menor = n1;
+   }
+   else if (n2 <= n1 && n2 <= n3)
+   {
+      menor = n2;
+   }
+   else if (n3 <= n1 && n3 <= n2)
+   {
+      menor = n3;
+   }
+
+   // Imprimir saída
+   printf("MENOR = %i\nMAIOR = %i", menor, maior);
+
    return 0;
 }
