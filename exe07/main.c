d4623e1db2f0832aa9778b52d03135e9d4b65485
Gabriel Arcanjo dos Santos Ferreira <gabriel.ferreira@bancointer.com.br>
2021-08-08 20:50:29 -0300

    Add exe07

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main()
+{
+   int n, maior, menor, num;
+
+   // Ler número de entradas e definir vetor
+   scanf("%i", &n);
+
+   // Ler as N entradas e calcular maior e mnor
+   for (int count = 0; count < n; count++)
+   {
+      scanf("%i", &num);
+      if (count == 0)
+      {
+         maior = num;
+         menor = num;
+      }
+      else
+      {
+         if (num > maior)
+         {
+            maior = num;
+         }
+         else if (num < menor)
+         {
+            menor = num;
+         }
+      }
+   }
+
+   // Imprimir saída
+   printf("MENOR = %i\nMAIOR = %i", menor, maior);
 }
